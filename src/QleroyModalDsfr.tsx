1217df20abcde9eec5a05f5d3f0c275ccf26612d
Quentin Leroy <quentin.n.leroy@gmail.com>
2024-04-14 18:12:44 +0200

    init

---
+++ src/QleroyModalDsfr.tsx
@@ -0,0 +1,87 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { useEffect, createRef } from 'react';
+import { styled } from '@superset-ui/core';
+import { QleroyModalDsfrProps, QleroyModalDsfrStylesProps } from './types';
+
+// The following Styles component is a <div> element, which has been styled using Emotion
+// For docs, visit https://emotion.sh/docs/styled
+
+// Theming variables are provided for your use via a ThemeProvider
+// imported from @superset-ui/core. For variables available, please visit
+// https://github.com/apache-superset/superset-ui/blob/master/packages/superset-ui-core/src/style/index.ts
+
+const Styles = styled.div<QleroyModalDsfrStylesProps>`
+height: 100%;
+width: 100%;
+`;
+
+/**
+ * ******************* WHAT YOU CAN BUILD HERE *******************
+ *  In essence, a chart is given a few key ingredients to work with:
+ *  * Data: provided via `props.data`
+ *  * A DOM element
+ *  * FormData (your controls!) provided as props by transformProps.ts
+ */
+
+export default function QleroyModalDsfr(props: QleroyModalDsfrProps) {
+  // height and width are the height and width of the DOM element as it exists in the dashboard.
+  // There is also a `data` prop, which is, of course, your DATA 🎉
+  const { height, width, modalTitle, modalDescription } = props;
+
+  const rootElem = createRef<HTMLDivElement>();
+
+  // Often, you just want to access the DOM and do whatever you want.
+  // Here, you can do that with createRef, and the useEffect hook.
+  useEffect(() => {
+    const root = rootElem.current as HTMLElement;
+    console.log('Plugin element', root);
+  });
+
+  console.log('Plugin props', props);
+
+  return (
+    <Styles
+      ref={rootElem}
+      height={height}
+      width={width}
+    >
+      <button className="fr-btn" data-fr-opened="false" aria-controls="fr-modal-1">
+        {modalTitle}
+      </button>
+      <dialog aria-labelledby="fr-modal-title-modal-1" role="dialog" id="fr-modal-1" className="fr-modal">
+        <div className="fr-container fr-container--fluid fr-container-md">
+          <div className="fr-grid-row fr-grid-row--center">
+            <div className="fr-col-12 fr-col-md-8 fr-col-lg-6">
+              <div className="fr-modal__body">
+                <div className="fr-modal__header">
+                  <button className="fr-btn--close fr-btn" title="Fermer la fenêtre modale" aria-controls="fr-modal-1">Fermer</button>
+                </div>
+                <div className="fr-modal__content">
+                  <h1 id="fr-modal-title-modal-1" className="fr-modal__title"><span className="fr-icon-arrow-right-line fr-icon--lg"></span>Titre de la modale</h1>
+                  <p>{modalDescription}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </Styles>
+  );
+}
